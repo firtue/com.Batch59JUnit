6448b151555c7f274faab53aa502b8b7ac7f63a9
A Erdem Ergin <a.erdemergin@gmail.com>
2022-05-14 19:39:24 +0300

    commit2

--- src/test/java/day06_radioButton_checkBox/C04_Assertions.java
+++ src/test/java/day06_radioButton_checkBox/C04_Assertions.java
@@ -2,6 +2,7 @@ package day06_radioButton_checkBox;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -29,6 +30,24 @@ public class C04_Assertions {
     public void test01(){
         // Eğer Test method'unuzda hiçbir test yoksa, test çalıştıktan sonra hiç bir problemle
         // karşılaşılmadığını raporlamak için "tests passed" yazısı çıkar
+        /*
+            Eğer testleri if ile yaparsak test failed olsa bile kodlar problemsiz çalıştığı için
+            kod çalışması bittiğinde ekranın sol alt kısmında test passed yazacaktır
+         */
+
         driver.get("https://www.amazon.com");
+
+        // url'in https://www.facebook.com olduğunu test edin
+        /*
+        if (driver.getCurrentUrl().equals("https://www.facebook.com")){
+            System.out.println("Url testi PASSED");
+        } else{
+            System.out.println("Url testi FAILED");
+        }
+         */
+
+        String expectedUrl="https://www.facebook.com";
+        String actualUrl= driver.getCurrentUrl();
+        Assert.assertEquals("Url beklenenden farklı çıktı",expectedUrl, actualUrl);
     }
 }
