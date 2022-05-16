69a5b5e345fcf907f768908b74a7462e0b6a9ee5
A Erdem Ergin <a.erdemergin@gmail.com>
2022-05-16 23:23:53 +0300

    commit3

--- src/test/java/day07_assertions/C05_DropSownOptions.java
+++ src/test/java/day07_assertions/C05_DropSownOptions.java
@@ -2,12 +2,17 @@ package day07_assertions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C05_DropSownOptions {
     /*
@@ -16,7 +21,7 @@ public class C05_DropSownOptions {
     sectigimiz option'i yazdiralim
 
     dropdown'daki opsiyonlarin toplam sayisinin
-    23 oldugunu test edin
+    28 oldugunu test edin
      */
     WebDriver driver;
 
@@ -36,6 +41,25 @@ public class C05_DropSownOptions {
     @Test
     public void test(){
         driver.get("https://www.amazon.com");
+        WebElement ddm= driver.findElement(By.xpath("//select[@class='nav-search-dropdown searchSelect nav-progressive-attrubute nav-progressive-search-dropdown'][1]"));
+
+        Select select= new Select(ddm);
+        select.selectByVisibleText("Books");
+
+        // bir dropdown ile çalışıyorken, son seçilen option'a ulaşmak isterseniz
+        // select.getFirstSelectedOption().getText() method'unu kullanmalısınız
+        // bu method bize WebElement döndürür,
+        // üzerindeki yazıyı yazdırmak için getText() unutulmamalıdır
+        System.out.println(select.getFirstSelectedOption().getText());
+
+
+        //dropdown'daki opsiyonlarin toplam sayisinin 28 oldugunu test edin
+        List<WebElement> optionList = select.getOptions();
+
+        int actualOptionSayisi = optionList.size();
+        int expectedOptionSayisi = 28;
+
+        Assert.assertEquals(actualOptionSayisi,expectedOptionSayisi);
 
     }
 }
