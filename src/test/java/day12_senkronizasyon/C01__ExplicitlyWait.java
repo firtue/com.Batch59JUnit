87e8bd8bc4ab175ed8a032937ee71b53dd52f6aa
A Erdem Ergin <a.erdemergin@gmail.com>
2022-05-23 14:56:11 +0300

    WaitTest

--- src/test/java/day12_senkronizasyon/C01__ExplicitlyWait.java
+++ src/test/java/day12_senkronizasyon/C01__ExplicitlyWait.java
@@ -1,30 +1,77 @@
 package day12_senkronizasyon;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
 
-public class C01__ExplicitlyWait extends TestBase {
-    @Test
-    public void implicitlyWaitTest(){
-        //1. Bir class olusturun : WaitTest
+import java.time.Duration;
 
-        //2. Iki tane metod olusturun : implicitWait() , explicitWait()
-        //     Iki metod icin de asagidaki adimlari test edin.
+public class C01__ExplicitlyWait extends TestBase {
+
+    //1. Bir class olusturun : WaitTest
+    //2. Iki tane metod olusturun : implicitWaitTesti() , explicitWaitTesti()
+    //     Iki metod icin de asagidaki adimlari test edin.
+
+    @Test
+    public void implicitWaitTesti(){
 
         //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
         driver.get("https://the-internet.herokuapp.com/dynamic_controls");
 
-        //4. Remove butonuna basin.
+        //4. Remove butonuna basin.         //p[@id='message']
         driver.findElement(By.xpath("//button[@type='button']")).click();
 
         //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
-
+        WebElement itsGoneYaziElementi = driver.findElement(By.xpath("//p[text()=\"It's gone!\"]"));
+        Assert.assertTrue(itsGoneYaziElementi.isDisplayed());
 
         //6. Add buttonuna basin
-
+        driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
 
         //7. It’s back mesajinin gorundugunu test edin
+        WebElement itsBackElementi= driver.findElement(By.xpath("//p[text()=\"It's back!\"]"));
+        Assert.assertTrue(itsBackElementi.isDisplayed());
+    }
+
+    @Test
+    public void explicitWaitTesti() {
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //4. Remove butonuna basin.         //p[@id='message']
+        driver.findElement(By.xpath("//button[@type='button']")).click();
+
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(15));      // wait objesi olusturuldu
+        /*
+        WebElement itsGoneYaziElementi = driver.findElement(By.xpath("//p[text()=\"It's gone!\"]"));
+                                                            // gorunur olmasini istedigimiz elemnti locate ettik
+        wait.until(ExpectedConditions.visibilityOf(itsGoneYaziElementi));           // elementi bekle
+        Assert.assertTrue(itsGoneYaziElementi.isDisplayed());                       // gorunur oldugunu test et
+
+        yazinin gorunur olmasini beklerken yazinin locate'ini kullanmak sorun olusturur
+        cunku henuz gorunmeyen bir yazinin locate edilmesi de mumkun olmayabilir
+        (HTML kodlari yazan developer farkli uygulamalar yapabilir)
+        Bu durumda bekleme islemi ve locate'i birlikte yapmaliyiz
+         */
+
+        WebElement itsGoneYaziElementi = wait.until(ExpectedConditions.
+                                            visibilityOfElementLocated(By.xpath("//p[text()=\"It's gone!\"]")));
+        Assert.assertTrue(itsGoneYaziElementi.isDisplayed());
+
+        //6. Add buttonuna basin
+        driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
+
+        //7. It’s back mesajinin gorundugunu test edin
+        WebElement itsBackElementi= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()=\"It's back!\"]")));
+
+        Assert.assertTrue(itsBackElementi.isDisplayed());
 
     }
+
 }
